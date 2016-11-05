51520945c37f5b08322d6b2b8c104e2dee191fd5
maha15 <i130115@nu.edu.pk>
2016-11-05 15:29:11 +0500

    Add files via upload

---
+++ encryption.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class encryption {
+
+	public encryption() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name;
+		System.out.println("Enter the name of user");
+		
+		Scanner input= new Scanner(System.in);
+		name= input.next();
+		
+		System.out.println("name of user is "+name);
+		
+		Convertor(name);
+
+	}
+	static int Convertor(String l){
+		int sum=0;
+		System.out.println("name of user is "+name);
+		
+		for(int i=0;i<l.length();i++)
+		{
+			int ascii = (int) l[i];
+			sum= sum + ascii;
+			System.out.println("name of user is "+ encrypt);
+			
+		}
+		return sum;
+	}
+
+}
